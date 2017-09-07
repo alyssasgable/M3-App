335ad813dd8e46bd625a24e039eaba0c7fbb13e1
Unknown <alyssasgable@gmail.com>
2017-09-07 15:04:32 -0500

    Made it so I have have custom table view cells
    Uploaded user's information to Firebase database

---
+++ M3/FeedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FeedViewController.swift
+//  M3
+//
+//  Created by Alyssa on 9/4/17.
+//  Copyright © 2017 Savage. All rights reserved.
+//
+
+import Foundation
---
+++ M3/Messages.swift
@@ -0,0 +1,9 @@
+//
+//  Messages.swift
+//  M3
+//
+//  Created by Alyssa on 9/1/17.
+//  Copyright © 2017 Savage. All rights reserved.
+//
+
+import Foundation
